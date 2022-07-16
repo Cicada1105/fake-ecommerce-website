ce371172db8f2811b885bd5be3e9cd4bcc3b121d
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2022-07-15 23:49:45 -0400

    Added Team Members to about page

--- router.js
+++ router.js
@@ -3,6 +3,38 @@ const Router = express.Router();
 // Include pug templating engine
 const pug = require("pug");
 
+
+// The following data would be stored in a database
+const teamMembers = [
+	{
+		id: 1,
+		image: {
+			src: "irene_moreno.jpeg",
+			alt: "Irene Moreno Profile Picture"
+		},
+		name: "Irene Moreno",
+		position: "Marketing Coordinator"
+	},
+	{
+		id: 2,
+		image: {
+			src: "marius_garcia.jpeg",
+			alt: "Marius Garcia Profile Picture"
+		},
+		name: "Marius Garcia",
+		position: "Software Engineer"
+	},
+	{
+		id: 3,
+		image: {
+			src: "rosalyn_taylor.jpeg",
+			alt: "Rosalyn Taylor Profile Picture"
+		},
+		name: "Rosalyn Taylor",
+		position: "CEO"
+	}
+]
+
 Router.get("/",(req,res) => {
 	const compiledFile = pug.compileFile("./views/home.pug");
 
@@ -11,7 +43,18 @@ Router.get("/",(req,res) => {
 Router.get("/about",(req,res) => {
 	const compiledFile = pug.compileFile("./views/about.pug");
 
-	res.send(compiledFile({ title: "About" }));
+
+	res.send(compiledFile({ title: "About", teamMembers }));
+});
+Router.get("/profile/:id", (req,res) => {
+	const compiledFile = pug.compileFile("./views/profile.pug");
+
+	// Retrieve the user id from the url
+	const id = req.params.id;
+	// Retrieve team member associated with the respective id
+	const teamMember = teamMembers[id - 1];
+
+	res.send(compiledFile({ title: `${teamMember["name"]}'s Profile'}`, teamMember }));
 });
 Router.get("/contact",(req,res) => {
 	const compiledFile = pug.compileFile("./views/contact.pug");
